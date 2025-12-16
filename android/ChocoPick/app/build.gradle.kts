3a07936bca42d96366352e1749b8966f01f3074d
DAHYUN <88223753+sondahyun@users.noreply.github.com>
2025-12-16 22:10:27 +0900

    [FE] chore: Firebase(Auth/Firestore/FCM) 초기 설정

--- android/ChocoPick/app/build.gradle.kts
+++ android/ChocoPick/app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -40,6 +41,12 @@ android {
 }
 
 dependencies {
+    implementation(platform("com.google.firebase:firebase-bom:34.7.0"))
+
+    implementation("com.google.firebase:firebase-auth")
+    implementation("com.google.firebase:firebase-firestore")
+    implementation("com.google.firebase:firebase-messaging")
+
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
--- android/ChocoPick/app/src/main/java/com/ssafy/chocopick/MainActivity.kt
+++ android/ChocoPick/app/src/main/java/com/ssafy/chocopick/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.ssafy.chocopick
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.messaging.FirebaseMessaging
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +18,10 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        FirebaseMessaging.getInstance().token.addOnSuccessListener {
+            Log.d("FCM", "token=$it")
+        }
+
     }
 }

a6bc1ced88f6bcef9b155d8718e9aa3ebe95feaa
2kihyuk <kihyuk5566@naver.com>
2025-12-20 19:31:28 +0900

    [FE] feat: 매장 선택 기능 시작

--- android/ChocoPick/app/src/main/java/com/ssafy/chocopick/ui/home/HomeFragment.kt
+++ android/ChocoPick/app/src/main/java/com/ssafy/chocopick/ui/home/HomeFragment.kt
@@ -5,20 +5,21 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AlertDialog
+import androidx.fragment.app.replace
 import com.ssafy.chocopick.R
+import com.ssafy.chocopick.databinding.FragmentHomeBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [HomeFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
+
 class HomeFragment : Fragment() {
-    // TODO: Rename and change types of parameters
+
+    private var _binding : FragmentHomeBinding? = null
+    private val binding get() = _binding!!
+
     private var param1: String? = null
     private var param2: String? = null
 
@@ -34,20 +35,45 @@ class HomeFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        _binding = FragmentHomeBinding.bind(view)
+        binding.storeChoiceBtn.setOnClickListener {
+            showStoreChoiceDialog()
+        }
+    }
+    fun showStoreChoiceDialog(){
+        val items = arrayOf("매장에서 선택", "목록에서 선택")
+        AlertDialog.Builder(requireContext())
+            .setTitle("매장 선택 방식")
+            .setItems(items){_,which ->
+                when(which) {
+                    0 -> {
+                        parentFragmentManager.beginTransaction()
+                            .replace(R.id.fragment_container,StoreMapFragment()).addToBackStack(null)
+                            .commit()
+                    }
+                    1 -> {
+                        parentFragmentManager.beginTransaction()
+                            .replace(R.id.fragment_container,StoreListFragment()).addToBackStack(null)
+                            .commit()
+                    }
+                }
+            }
+            .show()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment HomeFragment.
-         */
-        // TODO: Rename and change types and number of parameters
+
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
             HomeFragment().apply {

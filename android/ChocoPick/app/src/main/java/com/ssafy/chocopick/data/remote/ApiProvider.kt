7fabb1ed471eb7e4d2982fb3f41fad16b29e178a
kihyuk5566 <kihyuk5566@naver.com>
2025-12-23 09:29:03 +0900

    [FE] refactor: 주문 이후 장바구니 목록 화면으로 이동

--- android/ChocoPick/app/src/main/java/com/ssafy/chocopick/data/remote/ApiProvider.kt
+++ android/ChocoPick/app/src/main/java/com/ssafy/chocopick/data/remote/ApiProvider.kt
@@ -7,9 +7,10 @@ import retrofit2.converter.scalars.ScalarsConverterFactory
 
 
 object ApiProvider {
-    private const val BASE_URL = "http://175.120.137.147:8080/" // 에뮬레이터 -> PC localhost
+    private const val BASE_URL = "http://10.0.2.2:8080/" // 에뮬레이터 -> PC localhost
     //192.168.33.123
 //    10.0.2.2:8080
+//    175.120.137.147
     private val gson = GsonBuilder()
         .setLenient()
         .create()
--- android/ChocoPick/app/src/main/java/com/ssafy/chocopick/ui/order/CartFragment.kt
+++ android/ChocoPick/app/src/main/java/com/ssafy/chocopick/ui/order/CartFragment.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.widget.Toast
 import com.ssafy.chocopick.R
 import androidx.fragment.app.activityViewModels
+import androidx.fragment.app.replace
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
@@ -69,6 +70,8 @@ class CartFragment : Fragment(R.layout.fragment_cart) {
             lifecycleScope.launch {
                 saveOrderToFirebase(cartViewModel.cartItems.value)
             }
+            parentFragmentManager.beginTransaction().replace(R.id.fragment_container,
+                ProductListFragment()).commit()
         }
 
         binding.btnClear.setOnClickListener {
@@ -82,30 +85,30 @@ class CartFragment : Fragment(R.layout.fragment_cart) {
         cartViewModel.refresh()
     }
 
-    private fun requestDelayedFcmTest(){
-        FirebaseMessaging.getInstance().token
-            .addOnSuccessListener { token ->
-                viewLifecycleOwner.lifecycleScope.launch {
-                    runCatching {
-                        ApiProvider.fcmApi.sendDelayed(
-                            FcmRequestDto(
-                                token = token,
-                                title = "주문 완료!",
-                                body = "테스트 주문입니다"
-                            )
-                        )
-                    }.onSuccess {
-                        Toast.makeText(requireContext(), "Spring 요청 성공! (0/10/20초 알림)", Toast.LENGTH_SHORT).show()
-                    }.onFailure { e ->
-                        Toast.makeText(requireContext(), "Spring 요청 실패: ${e.message}", Toast.LENGTH_SHORT).show()
-                        Log.d("FCMTest","${e.message}")
-                    }
-                }
-            }
-            .addOnFailureListener { e ->
-                Toast.makeText(requireContext(), "FCM 토큰 획득 실패: ${e.message}", Toast.LENGTH_SHORT).show()
-            }
-    }
+//    private fun requestDelayedFcmTest(){
+//        FirebaseMessaging.getInstance().token
+//            .addOnSuccessListener { token ->
+//                viewLifecycleOwner.lifecycleScope.launch {
+//                    runCatching {
+//                        ApiProvider.fcmApi.sendDelayed(
+//                            FcmRequestDto(
+//                                token = token,
+//                                title = "주문 완료!",
+//                                body = "테스트 주문입니다"
+//                            )
+//                        )
+//                    }.onSuccess {
+//                        Toast.makeText(requireContext(), "Spring 요청 성공! (0/10/20초 알림)", Toast.LENGTH_SHORT).show()
+//                    }.onFailure { e ->
+//                        Toast.makeText(requireContext(), "Spring 요청 실패: ${e.message}", Toast.LENGTH_SHORT).show()
+//                        Log.d("FCMTest","${e.message}")
+//                    }
+//                }
+//            }
+//            .addOnFailureListener { e ->
+//                Toast.makeText(requireContext(), "FCM 토큰 획득 실패: ${e.message}", Toast.LENGTH_SHORT).show()
+//            }
+//    }
 
 
     private fun collectCart() {
@@ -180,7 +183,7 @@ class CartFragment : Fragment(R.layout.fragment_cart) {
     private fun sendFcmNotification() {
         FirebaseMessaging.getInstance().token
             .addOnSuccessListener { token ->
-                viewLifecycleOwner.lifecycleScope.launch {
+                lifecycleScope.launch {
                     runCatching {
                         ApiProvider.fcmApi.sendDelayed(
                             FcmRequestDto(
